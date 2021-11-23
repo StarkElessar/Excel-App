d3be41a8c9b1e2bd1ee1faeb401a37294de0a470
Stark <serj.elessar@gmail.com>
2021-11-23 22:47:40 +0300

    Started the fourth folder of the lesson

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,20 @@
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$element = document.querySelector(selector)
     this.components = options.components || []
+  }
+
+  getRoot() {
+    const $root = document.createElement('div')
+
+    this.components.forEach(Component => {
+      const component = new Component()
+    })
+
+    return $root
+  }
+
+  render() {
+    this.$element.append(this.getRoot())
   }
 }
